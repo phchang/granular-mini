0ea882178ad29c1b58ea7f4a29dcd40f96618968
Paul Chang <paul@phc.io>
2014-10-11 15:24:30 -0700

    whitespace clean-up

--- src/com/granular/model/WorkOrder.java
+++ src/com/granular/model/WorkOrder.java
@@ -1,33 +1,34 @@
 package com.granular.model;
 
 public class WorkOrder {
-    private Double targetQuantity;
-    private Double actualQuantityApplied;
-    private Status workOrderStatus;
+   private Double targetQuantity;
+   private Double actualQuantityApplied;
+   private Status workOrderStatus;
 
-    public WorkOrder() {}
+   public WorkOrder() {
+   }
 
-    public Double getTargetQuantity() {
-        return targetQuantity;
-    }
+   public Double getTargetQuantity() {
+      return targetQuantity;
+   }
 
-    public void setTargetQuantity(Double targetQuantity) {
-        this.targetQuantity = targetQuantity;
-    }
+   public void setTargetQuantity(Double targetQuantity) {
+      this.targetQuantity = targetQuantity;
+   }
 
-    public Double getActualQuantityApplied() {
-        return actualQuantityApplied;
-    }
+   public Double getActualQuantityApplied() {
+      return actualQuantityApplied;
+   }
 
-    public void setActualQuantityApplied(Double actualQuantityApplied) {
-        this.actualQuantityApplied = actualQuantityApplied;
-    }
+   public void setActualQuantityApplied(Double actualQuantityApplied) {
+      this.actualQuantityApplied = actualQuantityApplied;
+   }
 
-    public Status getWorkOrderStatus() {
-        return workOrderStatus;
-    }
+   public Status getWorkOrderStatus() {
+      return workOrderStatus;
+   }
 
-    public void setWorkOrderStatus(Status workOrderStatus) {
-        this.workOrderStatus = workOrderStatus;
-    }
+   public void setWorkOrderStatus(Status workOrderStatus) {
+      this.workOrderStatus = workOrderStatus;
+   }
 }
