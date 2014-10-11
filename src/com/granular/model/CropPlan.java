b6263d3dad2ffbde448a5ec8e1d0041b6d9e549e
Paul Chang <paul@phc.io>
2014-10-11 15:26:43 -0700

    filling out CropPlan

--- src/com/granular/model/CropPlan.java
+++ src/com/granular/model/CropPlan.java
@@ -1,4 +1,17 @@
 package com.granular.model;
 
+import java.util.List;
+
 public class CropPlan {
+   private List<Task> tasks;
+
+   public CropPlan() {}
+
+   public List<Task> getTasks() {
+      return tasks;
+   }
+
+   public void setTasks(List<Task> tasks) {
+      this.tasks = tasks;
+   }
 }
