57da66502cabd6dea571d016bd03954aff78403a
Paul Chang <paul@phc.io>
2014-10-11 15:26:26 -0700

    filling out Main

--- src/com/granular/Main.java
+++ src/com/granular/Main.java
@@ -1,8 +1,35 @@
 package com.granular;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-    }
+        /*
+            todo: plans
+               - list plans
+               - create plans
+               - add tasks to plan
+               - assign work orders to each task
+
+            todo: tasks
+               - add tasks
+
+            todo: work orders
+               - add work order
+         */
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      System.out.println("test input");
+
+      try {
+         String name = reader.readLine();
+         System.out.println("input = " + name);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 }
