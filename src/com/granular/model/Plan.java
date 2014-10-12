7f4572f9c9703e8576e3e9a74b65a0e3ed060e78
Paul Chang <paul@phc.io>
2014-10-12 07:48:54 -0700

    updating Plan fields

--- src/com/granular/model/Plan.java
+++ src/com/granular/model/Plan.java
@@ -1,11 +1,32 @@
 package com.granular.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Plan {
+   private Long id;
+   private String name;
    private List<Task> tasks;
 
-   public Plan() {}
+   public Plan() {
+      this.tasks = new ArrayList<>();
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
 
    public List<Task> getTasks() {
       return tasks;
@@ -13,5 +34,11 @@ public class Plan {
 
    public void setTasks(List<Task> tasks) {
       this.tasks = tasks;
+   }
+
+   public void addTask(Task task) {
+      // by default, not started
+      task.setTaskStatus(Status.NOT_STARTED);
+      this.tasks.add(task);
    }
 }
