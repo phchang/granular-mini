7d63709c4624a900bf8587b1cc710db98cfd8160
Paul Chang <paul@phc.io>
2014-10-12 16:00:49 -0700

    adding extra helper methods to the abstract class, as well as additional DAO interface methods

--- src/com/granular/dao/AbstractBasicDao.java
+++ src/com/granular/dao/AbstractBasicDao.java
@@ -1,23 +1,57 @@
 package com.granular.dao;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AbstractBasicDao<T> implements Dao<T> {
+public abstract class AbstractBasicDao<T> implements Dao<T> {
 
    private List<T> objects;
+   protected Connection conn;
 
    public AbstractBasicDao() {
       objects = new ArrayList<>();
    }
 
+   public AbstractBasicDao(Connection conn) {
+      this.conn = conn;
+      objects = new ArrayList<>();
+   }
+
+   // todo switch to PreparedStatement to prevent SQL injection
+
+   protected void executeUpdate(String update) throws SQLException {
+      PreparedStatement st = conn.prepareStatement(update);
+      st.executeUpdate(update);
+
+      st.close();
+   }
+
+   protected void executeQuery(String query, ResultSetHandler handler) throws SQLException {
+      Statement st;
+      ResultSet rs;
+
+      st = conn.createStatement();
+      rs = st.executeQuery(query);
+
+      handler.handle(rs);
+
+      rs.close();
+      st.close();
+   }
+
    @Override
-   public Iterable<T> findAll() {
+   public long getNextId() {
+      return objects.size() + 1;
+   }
+
+   @Override
+   public Iterable<T> findAll() throws SQLException {
       return new ArrayList<>(objects);
    }
 
    @Override
-   public void save(T obj) {
+   public void save(T obj) throws SQLException {
       objects.add(obj);
    }
 }
--- src/com/granular/dao/Dao.java
+++ src/com/granular/dao/Dao.java
@@ -1,9 +1,19 @@
 package com.granular.dao;
 
+import java.sql.SQLException;
+
 public interface Dao<T> {
 
-   Iterable<T> findAll();
+   long getNextId();
 
-   void save(T obj);
+   T findById(Long id) throws SQLException;
+
+   Iterable<T> findAll() throws SQLException;
+
+   void save(T obj) throws SQLException;
+
+   void update(T obj) throws SQLException;
+
+   void delete(T obj) throws SQLException;
 
 }
