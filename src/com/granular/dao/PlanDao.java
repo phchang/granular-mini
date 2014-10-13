6e73ab32dc9e38acc3ec4e2d790a9eeed3971ebf
Paul Chang <paul@phc.io>
2014-10-12 22:11:37 -0700

    adding a remove method

--- src/com/granular/dao/PlanDao.java
+++ src/com/granular/dao/PlanDao.java
@@ -4,4 +4,16 @@ import com.granular.model.Plan;
 
 public class PlanDao extends AbstractBasicDao<Plan> implements Dao<Plan> {
 
+   public void remove(Long id) {
+
+      Plan planToRemove = null;
+
+      for (Plan plan : objects) {
+         if (plan.getId().equals(id)) {
+            planToRemove = plan;
+         }
+      }
+
+      objects.remove(planToRemove);
+   }
 }
