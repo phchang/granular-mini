f181587c24ffab244b8f841544580644591eecb1
Paul Chang <paul@phc.io>
2014-10-12 22:12:27 -0700

    adding specific CRUD methods

--- src/com/granular/dao/InventoryDao.java
+++ src/com/granular/dao/InventoryDao.java
@@ -2,8 +2,40 @@ package com.granular.dao;
 
 import com.granular.model.Product;
 
-public class InventoryDao extends AbstractBasicDao<Product> implements Dao<Product>  {
+public class InventoryDao extends AbstractBasicDao<Product> implements Dao<Product> {
 
+   public void debitBalance(String productCode, Double amountToDebit) {
 
+      for (Product product : findAll()) {
+         if (product.getCode().equals(productCode)) {
+            product.setBalance(product.getBalance() - amountToDebit);
+         }
+      }
+   }
 
+   public boolean isAmountAvailable(String code, double applied) {
+      Product product = findByCode(code);
+
+      if (product != null) {
+         if (product.getBalance() > applied) {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   private Product findByCode(String code) {
+
+      Product prod = null;
+
+      for (Product product : findAll()) {
+         if (product.getCode().equals(code)) {
+            prod = product;
+            break;
+         }
+      }
+
+      return prod;
+   }
 }
